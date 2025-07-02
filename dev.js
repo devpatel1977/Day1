beebad9e742f5cd6b936edf29a8dc3d24f63e6a0
Dev Patel <devpatel25751977@gmail.com>
2025-07-02 18:33:54 +0530

    first commit

---
+++ dev.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const newE =<h1>Hollow dev patel</h1>
+const Reactroot = ReactDOM.createRoot(document.getElementById('root'));
+Reactroot.render(newE);
+
